5122398629d245f46689d7a63e5df38a5dead0f7
honglei <honglei_cn@126.com>
2017-06-13 23:49:56 +0800

    config页面的功能添加
    1.config的类型项目与数据库实时取数；
    2.config.php 更改为函数，直接提交动作通过函数处理返回json数据；

--- config.php
+++ config.php
@@ -1,13 +1,30 @@
 <?php
 header("Content-Type: text/html; charset=utf-8");
+error_reporting(E_ALL & ~E_NOTICE);
 
-    require_once 'connectvars.php';
+$action=$_GET['action'];
+$types = $_GET['types'];
+
+if($action == 'getItems'){
+    getItems();
+}else{
+
+	getData($types);
+}
+
+function getData($types){
+
+	require_once 'connectvars.php';
     @ $db = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 		if (mysqli_connect_error()) {
 		echo "could not connect to db";
 		exit;
 	}
-$sql = "SELECT * FROM `filter` WHERE 1";
+
+
+
+
+$sql = "SELECT * FROM `filter` WHERE `物料类型`='$types'";
 
 $get = $db -> query($sql);
 
@@ -21,5 +38,33 @@ $num_results = $get -> num_rows;
 
 
 	echo json_encode($results,JSON_UNESCAPED_UNICODE);
+
+}
+
+
+
+function getItems(){
+
+	require_once 'connectvars.php';
+    @ $db = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+		if (mysqli_connect_error()) {
+		echo "could not connect to db";
+		exit;
+	}
+
+$sql = "SELECT `物料类型` FROM `filter` WHERE 1 group by `物料类型`";
+
+$get = $db -> query($sql);
+
+$num_results = $get -> num_rows;
+
+  while($current = $get -> fetch_assoc()){
+			$results[] = $current;
+	}
+
+
+	echo json_encode($results,JSON_UNESCAPED_UNICODE);
+
+}
 
 ?>
--- others.php
+++ others.php
@@ -32,6 +32,14 @@ $sql= "SELECT `客户`, ROUND(sum(`销售数量(MT)`),2) as '销量(吨)',ROUND(
 
 }else if($filter == "客户利润排名"){
 $sql= "SELECT `客户`, ROUND(sum(`销售数量(MT)`),2) as '销量(吨)',ROUND(sum(`销售收入`)/10000,2) as '销售收入(万元)',ROUND(avg(`销售收入`)/avg(`销售数量(MT)`),2) as '吨均售价(元/吨)',ROUND(sum(`销售成本`)/10000,2) as '销售成本(万元)',ROUND(avg(`销售成本`)/avg(`销售数量(MT)`),2) as '吨均成本(元/吨)',ROUND(sum(`销售运费`)/10000,2) as '销售运费(万元)' ,ROUND(avg(`销售运费`)/avg(`销售数量(MT)`),2) as '吨均运费(元/吨)',ROUND(sum(`销售毛利`)/10000 ,2)as '销售毛利(万元)',ROUND(avg(`销售毛利`)/avg(`销售数量(MT)`),2) as '吨均毛利(元/吨)' from `maindata` where `物料描述` IN (select `物料` from `filter` where `物料类型`='$types') and `会计年度` between $yearFrom and $yearTo and `期间` between $monthFrom and $monthTo group by `客户` order by sum(`销售毛利`) DESC limit 5";
+}elseif($types == "散油"){
+ 
+ $sql = "SELECT `物料小小类`as '物料名称', ROUND(sum(`销售数量(MT)`),2) as '销量(吨)',ROUND(sum(`销售收入`)/10000,2) as '销售收入(万元)',ROUND(avg(`销售收入`)/avg(`销售数量(MT)`),2) as '吨均售价(元/吨)',ROUND(sum(`销售成本`)/10000,2) as '销售成本(万元)',ROUND(avg(`销售成本`)/avg(`销售数量(MT)`),2) as '吨均成本(元/吨)',ROUND(sum(`销售运费`)/10000,2) as '销售运费(万元)' ,ROUND(avg(`销售运费`)/avg(`销售数量(MT)`),2) as '吨均运费(元/吨)',ROUND(sum(`销售毛利`)/10000 ,2)as '销售毛利(万元)',ROUND(avg(`销售毛利`)/avg(`销售数量(MT)`),2) as '吨均毛利(元/吨)' from `maindata` where `物料描述` IN (select `物料` from `filter` where `物料类型`='豆油' or `物料类型`='棕榈油' or`物料类型`='菜油' or`物料类型`='棉油'or`物料类型`='玉米油' or `物料类型`='葵油') and `会计年度` between $yearFrom and $yearTo and `期间` between $monthFrom and $monthTo group by `物料小小类` ";
+
+
+}elseif($types == "粕"){
+	$sql = "SELECT `物料小小类` as '物料名称', ROUND(sum(`销售数量(MT)`),2) as '销量(吨)',ROUND(sum(`销售收入`)/10000,2) as '销售收入(万元)',ROUND(avg(`销售收入`)/avg(`销售数量(MT)`),2) as '吨均售价(元/吨)',ROUND(sum(`销售成本`)/10000,2) as '销售成本(万元)',ROUND(avg(`销售成本`)/avg(`销售数量(MT)`),2) as '吨均成本(元/吨)',ROUND(sum(`销售运费`)/10000,2) as '销售运费(万元)' ,ROUND(avg(`销售运费`)/avg(`销售数量(MT)`),2) as '吨均运费(元/吨)',ROUND(sum(`销售毛利`)/10000 ,2)as '销售毛利(万元)',ROUND(avg(`销售毛利`)/avg(`销售数量(MT)`),2) as '吨均毛利(元/吨)' from `maindata` where `物料描述` IN (select `物料` from `filter` where `物料类型`='棕榈粕' or `物料类型`='豆粕' or`物料类型`='菜饼' or`物料类型`='豆皮'or`物料类型`='其他') and `会计年度` between $yearFrom and $yearTo and `期间` between $monthFrom and $monthTo group by `物料小小类` ";
+
 }else $sql = "SELECT `$filter`, ROUND(sum(`销售数量(MT)`),2) as '销量(吨)',ROUND(sum(`销售收入`)/10000,2) as '销售收入(万元)',ROUND(avg(`销售收入`)/avg(`销售数量(MT)`),2) as '吨均售价(元/吨)',ROUND(sum(`销售成本`)/10000,2) as '销售成本(万元)',ROUND(avg(`销售成本`)/avg(`销售数量(MT)`),2) as '吨均成本(元/吨)',ROUND(sum(`销售运费`)/10000,2) as '销售运费(万元)' ,ROUND(avg(`销售运费`)/avg(`销售数量(MT)`),2) as '吨均运费(元/吨)',ROUND(sum(`销售毛利`)/10000 ,2)as '销售毛利(万元)',ROUND(avg(`销售毛利`)/avg(`销售数量(MT)`),2) as '吨均毛利(元/吨)' from `maindata` where `物料描述` IN (select `物料` from `filter` where `物料类型`='$types') and `会计年度` between $yearFrom and $yearTo and `期间` between $monthFrom and $monthTo group by `$filter`";
 
 
